5e1cffc1c4c8b9aa9930caa9cdaef0b06f783630
Nathaniel Volfango <nvolfango@gmail.com>
2019-11-09 01:49:07 +0000

    Update

---
+++ devcs/MyExercises/nv_tutorial06.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nv_tutorial06
+{
+	class nv_tutorial06_exercise01
+	{
+		public static void MainProgram()
+		{
+
+		}
+	}
+
+	class nv_tutorial06_exercise02
+	{
+		public static void MainProgram()
+		{
+
+		}
+	}
+}
